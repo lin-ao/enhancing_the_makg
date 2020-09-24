1d570f0e7e43feac59b022f93f4f73c40f50ecb2
lin-ao <58115732+lin-ao@users.noreply.github.com>
2020-09-24 10:55:44 +0200

    Delete 25.authororcid.py

--- 04.generate_knowledge_graph/25.authororcid.py
+++
@@ -1,7 +0,0 @@
-with open("00.entity_resolution/18.authors_with_orcid.txt", "r") as f:
-    with open("25.AuthorORCID.nt", "w") as g:
-        for line in f:
-            AuthorId = line.split("\t")[0]
-            ORCID = line.split("\t")[17]
-            g.write(f'<http://ma-graph.org/entity/{AuthorId}> <http://ma-graph.org/property/hasORCID> <{ORCID}>^^<http://www.w3.org/2001/XMLSchema#string> .\n')
-            
