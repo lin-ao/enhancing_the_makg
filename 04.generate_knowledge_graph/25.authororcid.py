6b6abeb8aadf71ab3724c7239aa627b3c204d182
lin-ao <58115732+lin-ao@users.noreply.github.com>
2020-09-24 10:55:59 +0200

    Add files via upload
    initial commit

---
+++ 04.generate_knowledge_graph/25.authororcid.py
@@ -0,0 +1,8 @@
+with open("00.entity_resolution/18.authors_with_orcid.txt", "r") as f:
+    with open("25.AuthorORCID.nt", "w") as g:
+        for line in f:
+            AuthorId = line.split("\t")[0]
+            ORCID = line.strip("\n").split("\t")[17]
+            if not ORCID == "":
+                g.write(f'<http://ma-graph.org/entity/{AuthorId}> <http://ma-graph.org/property/hasORCID> <{ORCID}>^^<http://www.w3.org/2001/XMLSchema#string> .\n')
+            
